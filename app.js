43141a2869b1f19596157b5edc6b65b24e6d3198
techieshruti <shrutichandra721@gmail.com>
2025-03-21 11:33:50 +0530

    add eventlistener and initialized variables

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const startScreen = document.getElementById("start-screen");
+    const startGame = document.getElementById("start_game");
+    const gameScreen = document.getElementById("game-screen");
+    const videoContainer = document.getElementById("video-container");
+    const inputGuess = document.getElementById("input_guess");
+    const feedback = document.getElementById("feedback");
+    const attempts = document.getElementById("attempts");
+    const playAgain = document.getElementById("play-again");
+}) 
+//This event listener ensures that the script runs only after the entire HTML document has been loaded
+
 let secretNumber;
 
 function startGame(guess)
